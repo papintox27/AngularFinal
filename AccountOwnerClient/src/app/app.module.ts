3f78213ec4b44fd3b5cc9bb2e0038707f0cb5f2f
papintox27 <crispm27@hotmail.com>
2023-06-22 12:30:00 -0600

    angular navigation and routing

--- AccountOwnerClient/src/app/app.module.ts
+++ AccountOwnerClient/src/app/app.module.ts
@@ -4,17 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from 'src/home/home.component';
+import { HomeComponent } from 'src/app/home/home.component';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { MenuComponent } from './menu/menu.component';
+import { NotFoundComponent } from './error-pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MenuComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CollapseModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
